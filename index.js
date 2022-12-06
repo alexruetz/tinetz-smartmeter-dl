75e88e795ac2611105a21316db512e1ceb627e00
Alexander Ruetz <alex.ruetz1@gmail.com>
2022-12-06 21:42:19 +0100

    change to fit new layout

--- index.js
+++ index.js
@@ -3,32 +3,28 @@ require('dotenv').config();
 
 (async () => {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
+    
     const page = await browser.newPage();
     await page.goto('https://kundenportal.tinetz.at/powercommerce/tinetz/fo/portal/loginProcess');
+    await page.click('#modalSave');
     await page.type("#login", process.env.USERNAME);
     await page.type("#password", process.env.PASSWORD);
     await Promise.all([
         page.click('#loginButton'),
         page.waitForNavigation({ waitUntil: 'networkidle0' }),
     ]);
-    await page.goto('https://kundenportal.tinetz.at/powercommerce/tinetz/fo/portal/consumptionDetails?meteringCode=' + process.env.METERING_CODE);
-    await page.click('#dateType_DAY');
-    const [dl] = await page.$x("//a[@title='Download Menü']");
-    if (dl) {
-        await dl.click();
-    } else {
-        console.log("Download Menü not found");
-    }
+    await page.goto('https://kundenportal.tinetz.at/powercommerce/tinetz/fo/portal/consumptionDetails?meteringCode=' + process.env.METERING_CODE, {timeout: 5000});
+    await new Promise(r => setTimeout(r, 5000));
+    
+    await page.waitForSelector('#icon-bar-dropdown', {
+        visible: true,
+      });
+    await page.click('.fa-caret-square-left');
+    //await page.click('#dateType_DAY');
+    await page.click('.fa-download');
     const [button] = await page.$x("//a[contains(., 'Export als CSV-Datei')]");
-    if (button) {
-        await Promise.all([
-             button.click(),
-             page.waitForNavigation({ waitUntil: 'networkidle0' }),
-            ]);
-    } else {
-        console.log("CSV download not found");
-    }
+    button.click()
     await delay(5000); 
     await browser.close();
 })();
